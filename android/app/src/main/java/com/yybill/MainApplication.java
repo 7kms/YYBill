3f0148a10a4b79ea4c8fcc07a3551ddf3e3123bb
baotang <tlyspa@qq.com>
2016-08-31 09:35:44 +0800

    fix realm bug

--- android/app/src/main/java/com/yybill/MainApplication.java
+++ android/app/src/main/java/com/yybill/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
+import io.realm.react.RealmReactPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -23,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),new RealmReactPackage()
       );
     }
   };
