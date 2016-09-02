8292e70860e9369aee56dda26200a34d72c6fa1f
baotang <tlyspa@qq.com>
2016-09-02 08:36:01 +0800

    color

--- BillApp/Utils/color.js
+++ BillApp/Utils/color.js
@@ -4,4 +4,22 @@
     autumn:'#fbbc05',
     grass:'#34a853'
 };
+export const iconColorArr = [
+    "#ffff00",
+    "#ffcc00",
+    "#ff9900",
+    "#ccff33",
+    "#cc99cc",
+    "#99ff66",
+    "#9933cc",
+    "#33ff33",
+    "#FF83FA",
+    "#EE799F",
+    "#D1EEEE",
+    "#8B008B",
+    "#66CDAA",
+    "#008B8B",
+    "#76EEC6"
+];
+
 export default color;
